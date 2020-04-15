b7b559072e0a7566d87066e760ccd86264ce6de0
Dmitry Andriyanov <dima117a@yandex-team.ru>
2020-04-15 23:23:59 +0300

    new testing library

--- test/components/Link.test.tsx
+++ test/components/Link.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 import { Provider } from 'react-redux';
-import { mount } from 'enzyme';
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
+import { fireEvent, render, RenderResult } from '@testing-library/react';
 import configureMockStore from 'redux-mock-store';
 
 import { Link, RouterContext } from '../../src';
@@ -12,7 +12,12 @@ const mockStore = configureMockStore();
 
 function render2(node: React.ReactNode) {
     const store = mockStore();
-    const link = mount(<Provider store={store}>{node}</Provider>);
+    const { container }: RenderResult = render(
+        <Provider store={store}>{node}</Provider>
+    );
+    const link = container.querySelector('a');
+
+    if (link === null) throw new Error('invalid render');
 
     return { link, store };
 }
@@ -22,23 +27,27 @@ describe('Link', () => {
         it('proper content', () => {
             const { link } = render2(<Link href="/aaa">ggghhhjjjjkkk</Link>);
 
-            expect(link.getDOMNode().innerHTML).is.eq('ggghhhjjjjkkk');
+            expect(link.innerHTML).is.eq('ggghhhjjjjkkk');
         });
 
         it('proper html content', () => {
-            const { link } = render2(<Link 
-                href="/aaa" 
-                dangerouslySetInnerHTML={{
-                    __html: '<strong>123</strong>'
-                }}/>);
+            const { link } = render2(
+                <Link
+                    href="/aaa"
+                    attrs={{ 'data-testid': 1234 }}
+                    dangerouslySetInnerHTML={{
+                        __html: '<strong>123</strong>',
+                    }}
+                />
+            );
 
-            expect(link.getDOMNode().innerHTML).is.eq('<strong>123</strong>');
+            expect(link.innerHTML).is.eq('<strong>123</strong>');
         });
 
         it('proper href', () => {
             const { link } = render2(<Link href="/aaa">111</Link>);
 
-            expect(link.getDOMNode().getAttribute('href')).is.eq('/aaa');
+            expect(link.getAttribute('href')).is.eq('/aaa');
         });
 
         it('proper attributes', () => {
@@ -50,42 +59,43 @@ describe('Link', () => {
                     attrs={{
                         id: 123,
                         xxx: 'yyy',
-                        'aria-label': 'test1'
+                        'aria-label': 'test1',
                     }}
-                />
+                >
+                    222
+                </Link>
             );
 
             // base
-            expect(link.getDOMNode().getAttribute('class')).is.eq('action');
-            expect(link.getDOMNode().getAttribute('target')).is.eq('fdasgsgs');
+            expect(link.getAttribute('class')).is.eq('action');
+            expect(link.getAttribute('target')).is.eq('fdasgsgs');
 
             // custom
-            expect(link.getDOMNode().getAttribute('id')).is.eq('123');
-            expect(link.getDOMNode().getAttribute('xxx')).is.eq('yyy');
-            expect(link.getDOMNode().getAttribute('aria-label')).is.eq('test1');
+            expect(link.getAttribute('id')).is.eq('123');
+            expect(link.getAttribute('xxx')).is.eq('yyy');
+            expect(link.getAttribute('aria-label')).is.eq('test1');
         });
     });
 
     describe('basename', () => {
-
         it('render url with basename', () => {
             const { link } = render2(
                 <RouterContext.Provider value={{ basename: 'mimi1' }}>
-                    <Link href="/bbb" />
+                    <Link href="/bbb">333</Link>
                 </RouterContext.Provider>
             );
 
-            expect(link.getDOMNode().getAttribute('href')).is.eq('/mimi1/bbb');
+            expect(link.getAttribute('href')).is.eq('/mimi1/bbb');
         });
 
         it("action's url without basename", () => {
             const { link, store } = render2(
                 <RouterContext.Provider value={{ basename: 'mimi2' }}>
-                    <Link href="/ccc" />
+                    <Link href="/ccc">444</Link>
                 </RouterContext.Provider>
             );
 
-            link.simulate('click', { button: 0 });
+            fireEvent.click(link, { button: 0 });
             const [action] = store.getActions();
 
             expect(action.url).is.eq('/ccc');
@@ -96,7 +106,7 @@ describe('Link', () => {
         it('proper action', () => {
             const { link, store } = render2(<Link href="/aaa" />);
 
-            link.simulate('click', { button: 0 });
+            fireEvent.click(link, { button: 0 });
             const [action] = store.getActions();
 
             expect(action.type).is.eq(
@@ -107,113 +117,127 @@ describe('Link', () => {
 
         it('handler runs on click', () => {
             let event: any;
-            const { link } = render2(<Link 
-                href="/aaa" 
-                onClick={(e) => { event = e }} />);
-    
-            link.simulate('click', { button: 0, clientX: 12, clientY: 14 });
-            
+            const { link } = render2(
+                <Link
+                    href="/aaa"
+                    onClick={({ button, clientX, clientY }) => {
+                        event = { button, clientX, clientY };
+                    }}
+                />
+            );
+
+            fireEvent.click(link, { button: 0, clientX: 12, clientY: 14 });
+
             expect(event.button).is.eq(0);
             expect(event.clientX).is.eq(12);
             expect(event.clientY).is.eq(14);
         });
 
         it('action not fired when preventDefault is called', () => {
-            const { link, store } = render2(<Link 
-                href="/aaa" 
-                onClick={(e) => { e.preventDefault(); }} />);
-    
-            link.simulate('click', { button: 0 });
+            const { link, store } = render2(
+                <Link
+                    href="/aaa"
+                    onClick={(e) => {
+                        e.preventDefault();
+                    }}
+                />
+            );
+
+            fireEvent.click(link, { button: 0 });
             const actions = store.getActions();
-            
+
             expect(actions).is.empty;
         });
 
         it('fire action on left button click', () => {
             const { link, store } = render2(<Link href="/aaa0" />);
 
-            link.simulate('click', { button: 0 });
+            fireEvent.click(link, { button: 0 });
             const [action] = store.getActions();
 
             expect(action.url).is.eq('/aaa0');
         });
 
-        it('don\'t fire action on middle button click', () => {
+        it("don't fire action on middle button click", () => {
             const { link, store } = render2(<Link href="/aaa1" />);
 
-            link.simulate('click', { button: 1 });
+            fireEvent.click(link, { button: 1 });
             const actions = store.getActions();
 
             expect(actions).is.empty;
         });
 
-        it('don\'t fire action on right button click', () => {
+        it("don't fire action on right button click", () => {
             const { link, store } = render2(<Link href="/aaa2" />);
 
-            link.simulate('click', { button: 2 });
+            fireEvent.click(link, { button: 2 });
             const actions = store.getActions();
 
             expect(actions).is.empty;
         });
 
-        it('don\'t fire action when link has target', () => {
+        it("don't fire action when link has target", () => {
             const { link, store } = render2(<Link href="/bbb" target="zzzz" />);
 
-            link.simulate('click', { button: 0 });
+            fireEvent.click(link, { button: 0 });
             const actions = store.getActions();
 
             expect(actions).is.empty;
         });
 
-        it('fire action when link target is \'_self\'', () => {
-            const { link, store } = render2(<Link href="/ccc" target="_self" />);
+        it("fire action when link target is '_self'", () => {
+            const { link, store } = render2(
+                <Link href="/ccc" target="_self" />
+            );
 
-            link.simulate('click', { button: 0 });
+            fireEvent.click(link, { button: 0 });
             const [action] = store.getActions();
 
             expect(action.url).is.eq('/ccc');
         });
 
-        it('don\'t fire action when Alt key is pressed', () => {
+        it("don't fire action when Alt key is pressed", () => {
             const { link, store } = render2(<Link href="/vvv1" />);
 
-            link.simulate('click', { button: 0, altKey: true });
+            fireEvent.click(link, { button: 0, altKey: true });
             const actions = store.getActions();
 
             expect(actions).is.empty;
         });
 
-        it('don\'t fire action when Ctrl key is pressed', () => {
+        it("don't fire action when Ctrl key is pressed", () => {
             const { link, store } = render2(<Link href="/vvv2" />);
 
-            link.simulate('click', { button: 0, ctrlKey: true });
+            fireEvent.click(link, { button: 0, ctrlKey: true });
             const actions = store.getActions();
 
             expect(actions).is.empty;
         });
 
-        it('don\'t fire action when Shift key is pressed', () => {
+        it("don't fire action when Shift key is pressed", () => {
             const { link, store } = render2(<Link href="/vvv3" />);
 
-            link.simulate('click', { button: 0, shiftKey: true });
+            fireEvent.click(link, { button: 0, shiftKey: true });
             const actions = store.getActions();
 
             expect(actions).is.empty;
         });
 
-        it('don\'t fire action when meta key is pressed', () => {
+        it("don't fire action when meta key is pressed", () => {
             const { link, store } = render2(<Link href="/vvv4" />);
 
-            link.simulate('click', { button: 0, metaKey: true });
+            fireEvent.click(link, { button: 0, metaKey: true });
             const actions = store.getActions();
 
             expect(actions).is.empty;
         });
 
-        it('don\'t fire action when link has forceReload attribute', () => {
-            const { link, store } = render2(<Link href="/bbbx" forceReload={true} />);
+        it("don't fire action when link has forceReload attribute", () => {
+            const { link, store } = render2(
+                <Link href="/bbbx" forceReload={true} />
+            );
 
-            link.simulate('click', { button: 0 });
+            fireEvent.click(link, { button: 0 });
             const actions = store.getActions();
 
             expect(actions).is.empty;
@@ -224,7 +248,7 @@ describe('Link', () => {
         it('empty values by default', () => {
             const { link, store } = render2(<Link href="/aaa0" />);
 
-            link.simulate('click', { button: 0 });
+            fireEvent.click(link, { button: 0 });
             const [action] = store.getActions();
 
             expect(action.replace).is.false;
@@ -232,9 +256,11 @@ describe('Link', () => {
         });
 
         it('replace passed from props', () => {
-            const { link, store } = render2(<Link href="/aaa0" replace={true} />);
+            const { link, store } = render2(
+                <Link href="/aaa0" replace={true} />
+            );
 
-            link.simulate('click', { button: 0 });
+            fireEvent.click(link, { button: 0 });
             const [action] = store.getActions();
 
             expect(action.replace).is.true;
@@ -242,9 +268,11 @@ describe('Link', () => {
 
         it('state passed from props', () => {
             const state = { x: 'y' };
-            const { link, store } = render2(<Link href="/aaa0" state={state} />);
+            const { link, store } = render2(
+                <Link href="/aaa0" state={state} />
+            );
 
-            link.simulate('click', { button: 0 });
+            fireEvent.click(link, { button: 0 });
             const [action] = store.getActions();
 
             expect(action.state).is.eq(state);
